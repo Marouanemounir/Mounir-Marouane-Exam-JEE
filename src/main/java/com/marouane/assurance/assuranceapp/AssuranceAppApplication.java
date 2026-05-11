df8fc39b2af880c4f9524775c119aff3b7922b0a
Marouane Mounir <mounirmarwane4@gmail.com>
2026-05-11 09:48:21 +0100

    backend initialisation

---
+++ src/main/java/com/marouane/assurance/assuranceapp/AssuranceAppApplication.java
@@ -0,0 +1,13 @@
+package com.marouane.assurance.assuranceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssuranceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssuranceAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marouane/assurance/assuranceapp/AssuranceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marouane.assurance.assuranceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssuranceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
